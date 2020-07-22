5289b6aa9de6a77e79d7b39a9e06855d14033c6d
luis sundar <sundarluis98@gmail.com>
2020-07-21 22:34:35 -0500

    finish js_funtions

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
-const {array,arrayObjects} = require('./exports/arraysObject')
-console.log("-- before--")
-console.log(array)
-console.log(arrayObjects)
-
-array.push("agregado")
-let newObject =  {
-    id:3,
-    name:"testName3",
-    lastName:"lastName3",
-    status:true
+function typeOne() {
+    return true
 }
-arrayObjects.push(newObject)
-
-console.log("-- after--")
-console.log(array)
-console.log(arrayObjects)
+console.log("typeOne", typeOne())
+/* *** */
+function typeOneWithParam(param) {
+    if (param) {
+        return true
+    } else {
+        return false
+    }
+}
+console.log("typeOneWithParam", typeOneWithParam("test"))
+/* *** */
+const newFormat = function () {
+    return true
+}
+console.log("newFormat", newFormat())
+/* *** */
+const newFormatWithParam = function (param) {
+    if (param) {
+        return true
+    } else {
+        return false
+    }
+}
+console.log("newFormatWithParam", newFormatWithParam("test"))
+/* *** */
+const arrowFunction = () => true
+console.log("arrowFunction", arrowFunction())
+/* *** */
+const arrowFunctionWithParam = (param) => param ? true : false
+console.log("arrowFunctionWithParam", arrowFunctionWithParam("true"))
