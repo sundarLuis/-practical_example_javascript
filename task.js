ab7c82c9bc7805755c5ffcb8eb5de3e43681a8e7
luis sundar <sundarluis98@gmail.com>
2020-07-22 15:58:03 -0500

    finish lodash

---
+++ task.js
@@ -0,0 +1,43 @@
+/* 
+   1 agrupar y ordenar  por lenguaje y edition si es que es el mismo author
+   2 solo a ese data necesitamos validar su estado (ACTIVO y NO ACTIVO) y guardar ese estado
+    en mayúscula si no tiene por defecto agregar (NO ACTIVO).
+*/
+//https://docs.google.com/document/d/1fWP7ll1s1c4upsNRh0BW5XlAfmgMbSBoea7p-KMEIus/edit?usp=sharing
+const task = [
+    {
+        "id":1,
+        "lenguaje":"java",
+        "edition":"tercera",
+        "author":"herbet schildt",
+        "status":"Activo"
+    },
+    {
+        "id":2,
+        "lenguaje":"java",
+        "edition":"primera",
+        "author":"herbet schildt",
+        "status":"Activo"
+    },
+    {
+        "id":3,
+        "lenguaje":"java",
+        "edition":"segunda",
+        "author":"herbet schildt",
+        "status":"Activo"
+    },
+    {
+        "id":4,
+        "lenguaje":"c++",
+        "edition":"segunda",
+        "author":"E.Balagurusamy",
+        "status":"No activo"
+    },
+    {
+        "id":5,
+        "lenguaje":"c++",
+        "edition":"primera",
+        "author":"E.Balagurusamy",
+        "status":"No activo"
+    }
+]
